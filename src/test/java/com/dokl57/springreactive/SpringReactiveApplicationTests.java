ff03e7d6a8bdbe3013bca6f0ee206e62250ef40d
Daniel Lykov <daniil2705@gmail.com>
2022-10-09 12:14:52 +0300

    Initial commit

---
+++ src/test/java/com/dokl57/springreactive/SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokl57.springreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
