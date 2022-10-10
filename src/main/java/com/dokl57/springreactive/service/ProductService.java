9b1c1d255c092a3300326abb14c70b0b4b8e9b5d
Daniel Lykov <daniil2705@gmail.com>
2022-10-10 15:45:49 +0300

    add save, update & delete methods

--- src/main/java/com/dokl57/springreactive/service/ProductService.java
+++ src/main/java/com/dokl57/springreactive/service/ProductService.java
@@ -6,11 +6,13 @@ import com.dokl57.springreactive.repository.ProductRepository;
 import com.dokl57.springreactive.util.AppUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Range;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@Service
 public class ProductService {
     private final ProductRepository productRepository;
 
@@ -29,6 +31,25 @@ public class ProductService {
 
     public Flux<ProductDto> getProductsInPriceRange(double min, double max){
         return productRepository.findByPriceBetween(Range.closed(min, max));
+    }
+
+    public Mono<ProductDto> saveProduct(Mono<ProductDto> productDtoMono){
+        return productDtoMono
+                .map(AppUtils::dtoToEntity)
+                .flatMap(productRepository::insert)
+                .map(AppUtils::entityToDto);
+    }
+
+    public Mono<ProductDto> updateProduct(Mono<ProductDto> productDtoMono, UUID id){
+        return productRepository.findById(id)
+                .flatMap(product -> productDtoMono.map(AppUtils::dtoToEntity)
+                .doOnNext(e -> e.setId(id)))
+                .flatMap(productRepository::save)
+                .map(AppUtils::entityToDto);
+    }
+
+    public Mono<Void> deleteProduct(UUID id){
+        return productRepository.deleteById(id);
     }
 
 
