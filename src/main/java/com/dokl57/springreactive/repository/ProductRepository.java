a7b53d5f41dee854b75d5e0d92e346175298751d
Daniel Lykov <daniil2705@gmail.com>
2022-10-10 15:45:27 +0300

    add annotation

--- src/main/java/com/dokl57/springreactive/repository/ProductRepository.java
+++ src/main/java/com/dokl57/springreactive/repository/ProductRepository.java
@@ -4,10 +4,12 @@ import com.dokl57.springreactive.dto.ProductDto;
 import com.dokl57.springreactive.entity.Product;
 import org.springframework.data.domain.Range;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends ReactiveMongoRepository<Product, UUID> {
 
     Flux<ProductDto> findByPriceBetween(Range<Double> priceRange);
