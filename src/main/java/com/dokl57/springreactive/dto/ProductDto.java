69acc3bd5cbdc1e95188bf13a902f43ca7113df6
Daniel Lykov <daniil2705@gmail.com>
2022-10-10 15:46:29 +0300

    add missing annotations

--- src/main/java/com/dokl57/springreactive/dto/ProductDto.java
+++ src/main/java/com/dokl57/springreactive/dto/ProductDto.java
@@ -3,12 +3,14 @@ package com.dokl57.springreactive.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Component
 public class ProductDto {
     private UUID id;
     private String name;
